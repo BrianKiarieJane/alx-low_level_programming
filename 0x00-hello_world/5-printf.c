b4f5f05f78e3b6e073e62026eb1f4b9c113554e7
Brian Kariuki <briankiariejane@gmail.com>
2022-12-08 04:27:41 -0800

    printf c program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - printf statement
+ * statement - "with proper grammar, but the outcome is a pice of art,"
+ * Return:0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
