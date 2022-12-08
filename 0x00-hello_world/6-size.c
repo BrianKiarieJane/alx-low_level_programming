3d417d2d1cdf49425ee239d9903e3d1749e88980
Brian Kariuki <briankiariejane@gmail.com>
2022-12-08 05:18:56 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,21 @@
 #include<stdio.h>
 /**
- * main-print sizes of different data sets
+ * main - print sizes of different data sets
  * data set order-char,int,long int,long long int,float
- * return :0
+ * return : 0
 */
 int main(void)
 {
-	printf("Size of char: %zu bytes\n", sizeof(char));
-	printf("Size of int: %zu bytes\n", sizeof(int));
-	printf("Size of long: %zu bytes\n", sizeof(long));
-	printf("Size of long long: %zu bytes\n", sizeof(long long));
-	printf("Size of float: %zu bytes\n", sizeof(float));
+	char w;
+	int x;
+	long y;
+	long long int z;
+	float a;
+
+	printf("Size of char: %lu byte(s)\n", (unsigned long) sizeof(w));
+	printf("Size of int: %lu byte(s)\n", (unsigned long) sizeof(x));
+	printf("Size of long: %lu byte(s)\n", (unsigned long) sizeof(y));
+	printf("Size of long long: %lu byte(s)\n", (unsigned long) sizeof(z));
+	printf("Size of float: %lu byte(s)\n", (unsigned long) sizeof(a));
 	return (0);
 }
