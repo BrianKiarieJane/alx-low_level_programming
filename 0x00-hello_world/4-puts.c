69b1a2f11efeeac1893892d4efed7069b9f5f99e
Brian Kariuki <briankiariejane@gmail.com>
2022-12-08 04:12:48 -0800

    put c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - puts a line that provides an output statement
+ * output statement - 'Programming is like building a multilingual puzzle'
+ * Return:0
+*/
+int main(void)
+{
+	puts('\"Programming is like building a multilingual puzzle');
+	return (0);
+}
