01692de805c76efb516df9c1c3dbd1513a4d8929
Brian Kariuki <briankiariejane@gmail.com>
2022-12-09 14:45:46 -0800

    alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 /**
- * main- print alphabet in lowercase
- * Return- 0
+ * main- Entry
+ * Return: Always 0(Success)
 */
 int main(void)
 {
 	char alphabet;
 
-	for (alphabet >= a; alphabet <= z; alphabet++)
+	for (alphabet >= 'a'; alphabet <= 'z'; alphabet++)
 	{
 		putchar(alphabet);
 	}
