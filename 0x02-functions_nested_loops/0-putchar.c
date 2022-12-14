bf1c432db43c3a3c5f7199d99ce95b74e2714aa0
Brian Kariuki <briankiariejane@gmail.com>
2022-12-14 12:45:41 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main-prints _putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int msg[] = {95, 112, 117, 116, 99, 104, 97, 114}
+	int count, x;
+
+	x = sizeof[msg]/size0f(int);
+	for (count=0; count<x; count++)
+	{
+		_putchar(msg[count]);
+	}
+	_putchar('\n');
+	return (0);
+}
