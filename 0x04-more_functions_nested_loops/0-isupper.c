d1a4e3250b770509e7f725acca09b5d11cf133a3
Brian Kariuki <briankiariejane@gmail.com>
2022-12-15 12:24:30 -0800

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _issuper - checks uppercase
+ * @c: input
+ * Return: 1 if upper otherwise 0
+ */
+ int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN-H
+#define MAIN_H
+int _isupper(int c);
+
+#endif
