11b3292252a4b45adf4a6663eed4ed39dc3da5f5
Brian Kariuki <briankiariejane@gmail.com>
2022-12-15 03:19:55 -0800

    102 fib

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,17 +6,18 @@
  */
 int main(void)
 {
-	int x;
-	long f1;
-	long f2;
-	long sum;
+	int c;
+	long int n1, n2, fn;
 
-	for (x = 0; x < 48; x++)
+	n1 = 1;
+	n2 = 2;
+	printf("%ld, %ld", n1, n2);
+	for (c = 0; c < 48; c++)
 	{
-		sum = f1 + f2;
-		printf(", %lu", sum);
-		f1 = f2;
-		f2 = sum;
+		fn = n1 + n2;
+		printf(", %ld", fn);
+		n1 = n2;
+		n2 = fn;
 	}
 	printf("\n");
 	return (0);
