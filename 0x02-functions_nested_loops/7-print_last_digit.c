f8ba421f5b7d6e406ee40348deb4de89a97d3311
Brian Kariuki <briankiariejane@gmail.com>
2022-12-14 16:31:51 -0800

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,12 +12,8 @@ int print_last_digit(int n)
 	x = n % 10;
 	if (x < 0)
 	{
-		_putchar(-x + n);
-		return (-x);
-	}
-	else
-	{
-		_putchar(x + n);
+		x *= -1;
+		_putchar(-x + '0');
 		return (x);
 	}
 }
