aec9449f03f7314489b3c7ef70311812c64d403f
Brian Kariuki <briankiariejane@gmail.com>
2022-12-15 13:58:52 -0800

    .

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * _issuper - checks uppercase
+ * _isupper - uppercase letters
  * @c: input
- * Return: 1 if upper otherwise 0
+ * Return: 0, 1
  */
- int _isupper(int c)
+int _isupper(int c)
+
 {
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
+
 }
