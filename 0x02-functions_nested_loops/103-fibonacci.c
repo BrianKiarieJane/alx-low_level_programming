e1a3e15f395f82ae030de4e737fdf6b2067314bb
Brian Kariuki <briankiariejane@gmail.com>
2022-12-15 02:39:55 -0800

    103 fib

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the sum of the even-valued terms
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	long f1;
+	long f2;
+	long sum;
+	float total;
+	
+	while(1)
+	{
+		f1 = 0;
+		f2 = 1;
+		sum = f1 + f2;
+
+		if (sum > 4000000)
+			break;
+
+		if ((sum % 2) == 0)
+			total += sum;
+		f1 = f2;
+		f2 = sum;
+	}
+	printf("%f\n", total);
+	return (0);
+}
