c56948505b4baf6c23306586bb5d6e206d282cf0
Brian Kariuki <briankiariejane@gmail.com>
2022-12-14 17:14:02 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add- adds two integers and returns the result
+ * @x: int 1
+ * @y: int 2
+ * Return: Results
+ */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = add(x, y);
+	_putchar(sum);
+	return (0);
+}
