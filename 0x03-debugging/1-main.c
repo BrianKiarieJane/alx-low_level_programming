658c3d89c93adaa9f526c322f175114406d7082d
Brian Kariuki <briankiariejane@gmail.com>
2022-12-15 12:06:18 -0800

    meet

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	int v;
+
+	printf("Infinite loop incoming :(\n");
+
+	v = 0;
+
+	/*while (v < 10)*/
+	/*{*/
+	/*putchar(v);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
