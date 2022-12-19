29925cd29533796a06a01a1ae55b363f2eb19a9f
Brian Kariuki <briankiariejane@gmail.com>
2022-12-19 14:38:02 -0800

    half prt

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.c"
+/**
+ * puts_half - prts halfstr
+ * @str: str
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+	int x, b;
+
+	x = 0;
+
+	while (str[x] != ' ')
+		x++;
+	if (x + 1 % 2 != '0')
+		b = (x - 1) / 2;
+	else
+		b = (x / 2);
+	b++;
+
+	for (x = b; str[x] != ' '; x++)
+		_putchar(str[x]);
+		_putchar('\n');
+}
