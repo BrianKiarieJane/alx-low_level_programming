059981a6cc1d49d1af9d052407969452355bd173
Brian Kariuki <briankiariejane@gmail.com>
2022-12-19 15:12:42 -0800

    .

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _atoi - str to int
+ * @s: pointer
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int u = v = w = 0;
+	int x = 1;
+
+	while (s[u])
+	{
+		if (s[u] == 45)
+		{
+			x *= -1;
+		}
+
+		while (s[u] >= 48 && s[u] <= 57)
+		{
+			w = 1;
+			v = (v * 10) + (s[u] - '0');
+			u++;
+		}
+		if (w == 1)
+		{
+			break;
+		}
+		u++;
+	}
+	v *= x;
+	return (v);
+}
