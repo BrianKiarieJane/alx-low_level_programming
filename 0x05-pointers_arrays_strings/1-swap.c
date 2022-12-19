81f53865bc82c8b5e187823ff063ccaeb488cc56
Brian Kariuki <briankiariejane@gmail.com>
2022-12-19 13:10:52 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*swap_int - swap fn
+*@a: int
+*@b: int
+* @*a: pointers
+* @*b: pointer
+* Return: 0(Success)
+*/
+void swap_int(int *a, int *b)
+{
+	int k;
+
+	k = *a;
+	*a = *b;
+	*b = k;
+	return (0);
+}
