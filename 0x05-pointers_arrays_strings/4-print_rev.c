c59b088ffbe45febb2c7c60eb2ee5491e17a166e
Brian Kariuki <briankiariejane@gmail.com>
2022-12-19 13:57:39 -0800

    rev str

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_rev - reverse
+ * @s: str
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	char *x;
+	int k;
+	int z;
+
+	k = 0;
+
+	while (s[k] != '\0')
+	{
+		k++;
+		*x = *s;
+		for (*x = *s)
+		{
+			*x = z;
+			while (z[k] = '\0')
+			{
+				_putchar(z[k]);
+				k++;
+			}
+		}
+	}
+	_putchar('\n');
+}
