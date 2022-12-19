bfdf1196e5582bad3a14e82cbc825766f22b10d2
Brian Kariuki <briankiariejane@gmail.com>
2022-12-19 14:26:51 -0800

    rv char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - prts char
+ * @str: str
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int q;
+
+	q = 0;
+
+	for (str[q]; str[q] != '\0'; q++)
+	{
+		if (q % 2 == 0)
+		{
+			_putchar(str[q]);
+		}
+	}
+	_putchar('\n');
+
+}
