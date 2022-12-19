cfde45029242616964a2e18088d8f82bd9c4a30f
Brian Kariuki <briankiariejane@gmail.com>
2022-12-19 14:50:25 -0800

    arr

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_array - prints array
+ * @a: pointer
+ * @n: int
+ * Return: 0
+ */
+void print_array(int *a, int n)
+{
+	int y;
+
+	y = 0;
+
+	while (y < n)
+	{
+		if (y != n - 1)
+		{
+			printf("%d, ", a[x]);
+		}
+		else
+		{
+			printf("%d", a[x]);
+		}
+		y++;
+	}
+	printf('\n');
