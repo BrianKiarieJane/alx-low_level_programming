2eaa1953eee45ecbf11e7ac9bccdbc6e26980793
Brian Kariuki <briankiariejane@gmail.com>
2022-12-21 13:54:12 -0800

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,15 +7,12 @@
  */
 void reverse_array(int *a, int n)
 {
-	int x, y, z;
+	int i, c;
 
-	for (x = 0; x < n - 1; X++)
+	for (i = 0; (i < (n - 1) / 2); i++)
 	{
-		for (y = 1; y > 0; y--)
-		{
-			z = *(a + y);
-			*(a + y) = *(a + (y - 1));
-			*(a + (y - 1)) = z;
-		}
+		c = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = c;
 	}
 }
