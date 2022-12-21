1889d3cefa390907eb41d1cd542c78671aaa704d
Brian Kariuki <briankiariejane@gmail.com>
2022-12-21 13:06:16 -0800

    task 2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_strncat - concatenates 2 str
+ * @dest: char
+ * @src: char
+ * @n: int
+ * Return: pointer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x;
+
+	for (x = 0; x < n && src[x] != '\0'; x++)
+	{
+		dest_end[x] = src[x];
+	}
+	dest_end[x] = '\0';
+	return dest;
+}
