cbb23a7cf6687f561bd474b3661480f15e9ae2b6
Brian Kariuki <briankiariejane@gmail.com>
2022-12-21 20:46:17 -0800

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *leet - encodes str
+ * @s: str
+ * Return: 0
+ */
+char *leet(char *s)
+{
+	int k;
+	int l;
+	char x[] = "aAeEoOtTlL";
+	char y[] = "4433007711";
+
+	for (k = 0; *(t + k); k++)
+	{
+		for (l = 0; l <= 9; l++)
+		{
+			if (x[l] == s[k])
+				t[k] = y[l];
+		}
+	}
+	return (s);
+}
