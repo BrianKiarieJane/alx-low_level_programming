4c50c81b98198ffd98a8627aa542406635f8a264
Brian Kariuki <briankiariejane@gmail.com>
2023-01-03 14:24:44 -0800

    tasak 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -16,6 +16,6 @@ char *_strchr(char *s, char c)
 	}
 	if (c == '\0')
 		return (s);
-	
-	return (0);
+
+	return (NULL);
 }
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * int _strspn - gets the length of a prefix substring
+ * @s: char
+ * @accept: char
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int x = 0, i = 0, j = 0;
+	
+	while (s[i] != 32)
+	{
+
+}
