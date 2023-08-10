9189ff858c67b44932452021c1a5ed73471badbd
Brian Kiarie Jane <BrianKiarieJane@gmail.com>
2023-08-10 15:43:17 -0700

    tasks completed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,12 @@
  */
 int main(void)
 {
+	long long int k;
+
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(k));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 	return (0);
 }
